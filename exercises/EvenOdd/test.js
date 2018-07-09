338abed68256ef64df4fa948e0032a1429d18a7b
polettit <tommaso.poletti.external@yoox.com>
2018-07-09 15:30:03 +0200

    Add new exercise

---
+++ exercises/EvenOdd/test.js
@@ -0,0 +1,28 @@
+const isEven = require('./index');
+
+describe('isEven function', () => {
+  test('should exists', () => {
+    expect(isEven).toBeDefined();
+    expect(typeof isEven).toEqual('function');
+  });
+  
+  test('234', () => {
+    expect(isEven(234)).toBeTruthy();
+  });
+  
+  test('-2', () => {
+    expect(isEven(-2)).toBeTruthy();
+  });
+  
+  test('33', () => {
+    expect(isEven(33)).toBeFalsy();
+  });
+  
+  test('3092348903.28409238409', () => {
+    expect(isEven(3092348903.28409238409)).toBeFalsy();
+  });
+  
+  test('.15', () => {
+    expect(isEven(.15)).toBeTruthy();
+  });
+});
