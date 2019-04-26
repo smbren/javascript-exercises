e7835944af006e0fc5554b846f347eae2d26bd9e
Scott Bren <smbren@ncsu.edu>
2019-04-26 12:12:12 -0400

    FizzBuzz complete

--- exercises/FizzBuzz/index.js
+++ exercises/FizzBuzz/index.js
@@ -18,6 +18,18 @@
 
 function fizzBuzz(n) {
 
+    for(i = 1; i <= n; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz");
+        } else if(i % 5 === 0) {
+            console.log("buzz");
+        } else if(i % 3 === 0) {
+            console.log("fizz");
+        } else {
+            console.log(i);
+        }
+    }
+
 }
 
 module.exports = fizzBuzz;
